d8d847b244f83b526e1153953087cd1ed89ed0c7
thanhloansqt <thanhloansqt@gmail.com>
2016-11-01 10:42:42 +0700

    Initialization

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
 # 2captcha
-This project is a folk from Mirio/captcha2upload
-Fix so that it can work with Python3
+* This project is a folk from Mirio/captcha2upload
+* Fix so that it can work with Python3
+
+# 2Captcha
+2Captcha team solves your CAPTCHA with high accuracy
+
+## How to install?
+
+### pip
+```
+pip install 2captcha
+```
+
+### Source
+```
+git clone https://github.com/nikoladang/2captcha.git
+cd 2captcha
+python setup.py install
+```
+
+## How to start?
+[Register Here](http://2captcha.com/?from=2529829)
+And get the api key:
+* Login in your account
+* Go to "2Captcha API"
+* Get "CAPTCHA Key"
+
+# Example
+
+## Solve Captcha
+```
+from 2captcha import CaptchaUpload
+
+captcha = CaptchaUpload(<YOURKEY>)
+print captcha.solve(<PATHFILE>)
+```
+
+## Get balance
+```
+from 2captcha import CaptchaUpload
+
+captcha = CaptchaUpload(<YOURKEY>)
+print captcha.getbalance()
+```
+
+## Attach the logs
+```
+from 2captcha import CaptchaUpload
+
+captcha = CaptchaUpload(<YOURKEY>, log=<YOURLOGVAR>)
+print captcha.getbalance()
+```
+
+## Change Wait Time
+```
+from 2captcha import CaptchaUpload
+
+captcha = CaptchaUpload(<YOURKEY>, waittime=<YOURLOGVAR>)
+print captcha.solve(<PATHFILE>)
+```
---
+++ setup.py
@@ -0,0 +1,46 @@
+##http://peterdowns.com/posts/first-time-with-pypi.html
+	#$python setup.py register -r pypitest
+	#$python setup.py sdist upload -r pypitest
+	#$python setup.py register -r pypi
+	#$python setup.py sdist upload -r pypi
+	#$python setup.py register -r pypitest && python setup.py sdist upload -r pypitest && python setup.py register -r pypi && python setup.py sdist upload -r pypi && pip install 2captcha --upgrade
+##https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+INSTALL_REQUIRES = ["requests >= 2.11.1"]
+
+from distutils.core import setup
+
+if __name__ == "__main__":
+    setup(
+      name = '2captcha',
+      packages = ['2captcha'], # this must be the same as the name above
+      version = '0.0.1',
+      description = 'An upload to 2captcha.com library',
+      author = 'Nikola Dang',
+      author_email = 'ducthinhdt@gmail.com',
+      url = 'https://github.com/nikoladang/2captcha', # use the URL to the github repo
+      download_url = 'https://github.com/nikoladang/2captcha/tarball/0.1', # I'll explain this in a second
+      keywords = ['2captcha', 'captcha', 'uploading', 'automation', 'resolving', 'automate'], # arbitrary keywords
+      classifiers = CLASSIFIERS,
+      install_requires=INSTALL_REQUIRES,
+    )
